3db98d32e795ae115d301251b4e9e5f4626af8a6
pvermot <pierre.vermot@obspm.fr>
2026-01-15 11:45:08 +0100

    Creating a more generic "VLT" instrument

---
+++ instruments/vlt/params_inst.py
@@ -0,0 +1,28 @@
+# Image parameters
+
+n_pix = 128
+n_frames = 32
+lam = 4.78e-6#3.80e-6 #4.78e-6 #2.18e-6i
+pixel_scale = 0.02719/206265#0.01225/206265#0.01225/206265
+
+# Pupil parameters
+
+upscaling = 32
+D=8.2
+D2=1.116
+
+# Randomness parameters
+
+max_n_tau = 30
+
+power_range = [0, -2]
+
+rms_max = lam/5
+
+snr_min = 0
+snr_max = 50
+
+
+# Compute mode
+
+compute_mode = "GPU"
